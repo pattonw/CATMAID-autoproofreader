303ad7f10a0f57075b8a2fb63dca985d8c09e6a5
pattonw <pattonw@hhmi.org>
2019-04-25 15:37:02 -0400

    change names

--- floodfilling/tests/test_commands.py
+++ floodfilling/tests/test_commands.py
@@ -5,12 +5,12 @@ from floodfilling.tests.common import FloodfillingTestCase
 
 
 class CommandsTests(FloodfillingTestCase):
-    def assert_ss_db_population_state(self, should_exist):
+    def assert_db_population_state(self, should_exist):
         assertion = self.assertTrue if should_exist else self.assertFalse
-        for ss_model in apps.get_app_config('floodfilling').get_models():
-            assertion(ss_model.objects.all().exists())
+        for extensions_model in apps.get_app_config("floodfilling").get_models():
+            assertion(extensions_model.objects.all().exists())
 
-    def test_clear_ss_tables(self):
-        self.assert_ss_db_population_state(True)
-        call_command('clear_ss_tables', '-y')
-        self.assert_ss_db_population_state(False)
+    def test_reset_floodfilling(self):
+        self.assert_db_population_state(True)
+        call_command("reset_floodfilling", "-y")
+        self.assert_db_population_state(False)
