401603df13bc052e282575d2fce2e84c59bbfa09
pattonw <pattonw@hhmi.org>
2019-04-26 13:04:54 -0400

    put expected results in list

--- autoproofreader/tests/apis/test_computeservers.py
+++ autoproofreader/tests/apis/test_computeservers.py
@@ -46,18 +46,18 @@ class ComputeServerTest(AutoproofreaderTestCase):
         )
         self.assertEqual(response.status_code, 200)
         parsed_response = json.loads(response.content.decode("utf-8"))
-        expected_result = {
-            "name": "test_server_1",
-            "address": "test_server_1.org",
-            "edition_time": "2001-01-01T01:01:01.001Z",
-            "diluvian_path": "test_1_diluvian",
-            "results_directory": "test_1_results",
-            "environment_source_path": "test_1_env",
-            "editor_id": 3,
-            "id": 1,
-        }
-        for k, v in expected_result.items():
-            self.assertEqual(v, parsed_response[k])
+        expected_result = [
+            {
+                "name": "test_server_1",
+                "address": "test_server_1.org",
+                "edition_time": "2001-01-01T01:01:01.001Z",
+                "diluvian_path": "test_1_diluvian",
+                "results_directory": "test_1_results",
+                "environment_source_path": "test_1_env",
+                "editor_id": 3,
+                "id": 1,
+            }
+        ]
         self.assertEqual(expected_result, parsed_response)
 
     def test_post(self):
