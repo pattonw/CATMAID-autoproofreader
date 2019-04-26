e5c73db8e7ed6953222d5d814402b7d695bd1c21
pattonw <pattonw@hhmi.org>
2019-04-26 11:20:36 -0400

    improve test

--- autoproofreader/tests/apis/test_computeservers.py
+++ autoproofreader/tests/apis/test_computeservers.py
@@ -1,3 +1,4 @@
+import json
 from autoproofreader.tests.common import AutoproofreaderTestCase
 
 URL_PREFIX = "/ext/autoproofreader"
@@ -6,10 +7,33 @@ URL_PREFIX = "/ext/autoproofreader"
 class ComputeServerTest(AutoproofreaderTestCase):
     def test_get(self):
         response = self.client.get(
-            "/{project_id}/compute-servers".format(**{"project_id": 0})
+            "/{project_id}/compute-servers".format(
+                **{"project_id": self.test_project_id}
+            )
         )
-        raise Exception(str(response))
         self.assertEqual(response.status_code, 200)
+        parsed_response = json.loads(response.content.decode("utf-8"))
+        expected_result = [
+            {
+                "name": "test_server_1",
+                "address": "test_server_1.org",
+                "edition_time": "2001-01-01T01:01:01.001Z",
+                "diluvian_path": "test_1_diluvian",
+                "results_directory": "test_1_results",
+                "environment_source_path": "test_1_env",
+                "editor": 1,
+            },
+            {
+                "name": "test_server_2",
+                "address": "test_server_2.org",
+                "edition_time": "2002-02-02T02:02:02.002Z",
+                "diluvian_path": "test_2_diluvian",
+                "results_directory": "test_2_results",
+                "environment_source_path": "test_2_env",
+                "editor": 1,
+            },
+        ]
+        self.assertEqual(expected_result, parsed_response)
 
     def test_post(self):
         raise NotImplementedError
