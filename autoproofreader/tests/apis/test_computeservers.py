ca2015ede9f95dd00aa4d5d7f5b3b62c901e170c
pattonw <pattonw@hhmi.org>
2019-04-26 11:30:20 -0400

    add test for single compute server

--- autoproofreader/tests/apis/test_computeservers.py
+++ autoproofreader/tests/apis/test_computeservers.py
@@ -35,6 +35,25 @@ class ComputeServerTest(AutoproofreaderTestCase):
         ]
         self.assertEqual(expected_result, parsed_response)
 
+        response = self.client.get(
+            "{url_prefix}/{project_id}/compute-servers".format(
+                **{"url_prefix": URL_PREFIX, "project_id": self.test_project_id}
+            ),
+            {"server_id": 1},
+        )
+        self.assertEqual(response.status_code, 200)
+        parsed_response = json.loads(response.content.decode("utf-8"))
+        expected_result = {
+            "name": "test_server_1",
+            "address": "test_server_1.org",
+            "edition_time": "2001-01-01T01:01:01.001Z",
+            "diluvian_path": "test_1_diluvian",
+            "results_directory": "test_1_results",
+            "environment_source_path": "test_1_env",
+            "editor": 1,
+        }
+        self.assertEqual(expected_result, parsed_response)
+
     def test_post(self):
         raise NotImplementedError
 
