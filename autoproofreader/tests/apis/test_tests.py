52128e1b7b62dc681e9e85a3f624792ab4cb1ee8
pattonw <pattonw@hhmi.org>
2019-04-29 15:02:19 -0400

    split into more tests

--- autoproofreader/tests/apis/test_tests.py
+++ autoproofreader/tests/apis/test_tests.py
@@ -6,7 +6,7 @@ TEST_URL = "/ext/autoproofreader/{}/tests"
 
 
 class DiluvianModelTest(AutoproofreaderTestCase):
-    def test_get(self):
+    def test_get_no_type(self):
         self.fake_authentication()
 
         response = self.client.get(
@@ -17,9 +17,12 @@ class DiluvianModelTest(AutoproofreaderTestCase):
         expected_result = {
             "data": {},
             "POST": {},
-            "query_params": {"greeting": "hello world", "time": 1200},
+            "query_params": {"greeting": "hello world", "time": "1200"},
         }
         self.assertEqual(expected_result, parsed_response)
+
+    def test_get_json_type(self):
+        self.fake_authentication()
 
         response = self.client.get(
             TEST_URL.format(self.test_project_id),
@@ -30,11 +33,11 @@ class DiluvianModelTest(AutoproofreaderTestCase):
         expected_result = {
             "data": {},
             "POST": {},
-            "query_params": {"greeting": "hello world", "time": 1200},
+            "query_params": {"greeting": "hello world", "time": "1200"},
         }
         self.assertEqual(expected_result, parsed_response)
 
-    def test_put(self):
+    def test_put_no_type(self):
         self.fake_authentication()
 
         response = self.client.put(
@@ -45,9 +48,12 @@ class DiluvianModelTest(AutoproofreaderTestCase):
         expected_result = {
             "data": {},
             "POST": {},
-            "query_params": {"greeting": "hello world", "time": 1200},
+            "query_params": {"greeting": "hello world", "time": "1200"},
         }
         self.assertEqual(expected_result, parsed_response)
+
+    def test_put_json_type(self):
+        self.fake_authentication()
 
         response = self.client.put(
             TEST_URL.format(self.test_project_id),
@@ -58,11 +64,11 @@ class DiluvianModelTest(AutoproofreaderTestCase):
         expected_result = {
             "data": {},
             "POST": {},
-            "query_params": {"greeting": "hello world", "time": 1200},
+            "query_params": {"greeting": "hello world", "time": "1200"},
         }
         self.assertEqual(expected_result, parsed_response)
 
-    def test_delete(self):
+    def test_delete_no_type(self):
         self.fake_authentication()
 
         response = self.client.delete(
@@ -73,9 +79,12 @@ class DiluvianModelTest(AutoproofreaderTestCase):
         expected_result = {
             "data": {},
             "POST": {},
-            "query_params": {"greeting": "hello world", "time": 1200},
+            "query_params": {"greeting": "hello world", "time": "1200"},
         }
         self.assertEqual(expected_result, parsed_response)
+
+    def test_delete_json_type(self):
+        self.fake_authentication()
 
         response = self.client.delete(
             TEST_URL.format(self.test_project_id),
@@ -86,6 +95,6 @@ class DiluvianModelTest(AutoproofreaderTestCase):
         expected_result = {
             "data": {},
             "POST": {},
-            "query_params": {"greeting": "hello world", "time": 1200},
+            "query_params": {"greeting": "hello world", "time": "1200"},
         }
         self.assertEqual(expected_result, parsed_response)
